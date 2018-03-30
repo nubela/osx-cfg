d817a8cd5bb60e1a8d908e1ba50aa0fe804f66d2
nubela <nubela@gmail.com>
2018-03-30 14:14:44 +0800

    updated import

--- karabiner/karabiner-import.sh
+++ karabiner/karabiner-import.sh
@@ -4,35 +4,43 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set custom.matias.ctrlr_to_b 1
 /bin/echo -n .
+$cli set general.dont_remap_non_apple_pointing 1
+/bin/echo -n .
+$cli set general.dont_remap_thirdvendor_pointing 1
+/bin/echo -n .
+$cli set option.drop_slowexpose 1
+/bin/echo -n .
 $cli set remap.control_delete_to_option_delete_winstyle 1
 /bin/echo -n .
-$cli set remap.controlArrow2optionLarrow 1
+$cli set remap.controlL2fn 1
 /bin/echo -n .
-$cli set remap.copy_paste_winstyle_new_tab 1
+$cli set remap.copy_paste_winstyle_new_browser_tab 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_no_term 1
 /bin/echo -n .
 $cli set remap.find_winstyle_no_term 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f1 1
-/bin/echo -n .
-$cli set remap.fkeys_to_consumer_f10 1
-/bin/echo -n .
-$cli set remap.fkeys_to_consumer_f3_lion 1
-/bin/echo -n .
-$cli set remap.fkeys_to_consumer_f5 1
-/bin/echo -n .
-$cli set remap.fkeys_to_consumer_f7 1
-/bin/echo -n .
-$cli set remap.functional2fn_2008 1
+$cli set remap.fn2controlL 1
 /bin/echo -n .
 $cli set remap.new_winstyle_no_term 1
 /bin/echo -n .
+$cli set remap.pc_keyboard_function_standard 1
+/bin/echo -n .
 $cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.pclikecontrolarrow_except_vm 1
+/bin/echo -n .
+$cli set remap.pclikehomeend 1
 /bin/echo -n .
 $cli set remap.pclikehomeend_emacs 1
 /bin/echo -n .
-$cli set remap.pcstyle_close 1
+$cli set remap.pclikepageupdown 1
 /bin/echo -n .
-$cli set remap.redo_winstyle_no_term 1
+$cli set remap.pclikepageupdown_except_emacs 1
+/bin/echo -n .
+$cli set remap.pcstyle_changeinputmethod2 1
+/bin/echo -n .
+$cli set remap.pointing_controlLeftclick2middleclick 1
 /bin/echo -n .
 $cli set remap.save_winstyle_no_term 1
 /bin/echo -n .
